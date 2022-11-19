1a5effa21311bca06c79ec1ffeaa474bc7eb3a3a
ANu anu <116893402+anubhabhalder18@users.noreply.github.com>
2022-11-19 11:34:24 +0530

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,12 +1,15 @@
-from tabulate import tabulate
+from tabulate import tabulate  #To use tables in python
 
-max=0
-row=[["Serial no.","NAME","PHONE NO","ADDRESS"]]
+max=0 #When producing new tables it will tell us how many times we have to append datas in a row
+row=[["Serial no.","NAME","PHONE NO","ADDRESS"]]  #1ST ROW OF TABLE
+
+#A class is created to store details of individual like name , phone ,address
 class phonebook:
   def __init__(self,name,phone,address):
     self.name=name
     self.phone=phone
     self.address=address
+  #A method that would add the datas of class in a row of table(ie. x)
   def myfunc(p):
     i=1
     global row
@@ -18,7 +21,9 @@ class phonebook:
     row.append(x)
     i+=1
 
+ #A function to customise tables
 def custom(a,b):
+  #COMMITING CHANGES ACCORDING TO CHOICES
   if(b==1):
     p[a-1].name=input("Enter new name\t")
     
@@ -42,11 +47,12 @@ def custom(a,b):
     
       else:
         ser=100
-
+#GENERATING 100 INDIVIDUAL ENTRIES to store data in the phonebook class
 p=[]
 for ele in range(100):
   p.append("p"+str(ele))
 
+#TAKING DATAS TO ADD IN PHONEBOOK
 ser=0
 while(ser<100):
   name=input("Enter name\t")
@@ -62,12 +68,13 @@ while(ser<100):
   else:
     ser=100
   
-
+#PUTTING A FLAG WHICH TELLS SYSTEM WHEN TO MAKE TABLES AND WHEN TO STOP
 flag=True
 while(flag==True):
-  row=[["Serial no.","NAME","PHONE NO","ADDRESS"]]
-  print("\tPHONEBOOK\n\n")
+  row=[["Serial no.","NAME","PHONE NO","ADDRESS"]]#RENEWING THE  2D TABLE WITH ONLY FIRST ROW
+  print("\tPHONEBOOK\n\n")#TABLE HEADING 
   num=0
+  
   for xe in range(max):
     p[num].myfunc()
   print(tabulate(row))
